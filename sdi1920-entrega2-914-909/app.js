873d30ed6542b421a6ccb7159b35f546d380070e
Nerea Valdés Egocheaga <nerevalego@gmail.com>
2020-05-10 04:23:19 +0200

    Servicios Web-S4 y Aplicacion Jquery-C3 Actualizado

--- sdi1920-entrega2-914-909/app.js
+++ sdi1920-entrega2-914-909/app.js
@@ -68,7 +68,8 @@ routerUsuarioToken.use(function (req, res, next) {
 // Aplicar routerUsuarioToken
 app.use('/api/amigos', routerUsuarioToken);
 app.use('/api/mensaje/:destino', routerUsuarioToken);
-app.use('/api/mensajes/:id', routerUsuarioToken);
+app.use('/api/conversacion/:id', routerUsuarioToken);
+app.use('/api/mensaje/leido/:id', routerUsuarioToken);
 
 let gestorDB = require("./modules/gestorDB.js");
 gestorDB.init(app, mongo);
--- sdi1920-entrega2-914-909/routes/rapiredsocial.js
+++ sdi1920-entrega2-914-909/routes/rapiredsocial.js
@@ -27,6 +27,7 @@ module.exports = function (app, gestorBD) {
             }
        });
     });
+    // S2 Mostrar amigos
     app.get("/api/amigos", function (req, res) {
         if (req.headers['token']) {
             let criterio = {
@@ -77,6 +78,56 @@ module.exports = function (app, gestorBD) {
             })
         }
     });
+    // S4 Mostrar mensajes de una conversación
+    app.get("api/conversacion/:id",function(req,res){
+        if (req.headers['token']) {
+            // Primero obtener usuario por _id -> amigo.email -> obtener mensajes
+            let criterioAmigo = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
+            gestorBD.obtenerUsuarios(criterioAmigo, function (usuarios) {
+                if (usuarios == null) {
+                    app.get("logger").error("Se ha producido un error al obtener al amigo de la API");
+                    res.status(500);
+                    res.json({
+                        error: "Se ha producido un error al obtener al amigo de la API"
+                    })
+                } else {
+                    let amigo = usuarios[0].email;
+                    let criterio = {
+                        $or: [
+                            {
+                                $and: [
+                                    {destino: res.usuario.email},
+                                    {emisor: amigo}
+                                ]
+                            },
+                            {
+                                $and: [
+                                    {destino: amigo},
+                                    {emisor: res.usuario.email}
+                                ]
+                            }
+                        ]
+                    };
+                    gestorBD.obtenerMensajes(criterio, function (mensajes) {
+                        if (mensajes == null) {
+                            app.get("logger").error("Se ha producido un error al obtener los mensajes de la API");
+                            res.status(500);
+                            res.json({
+                                error: "Se ha producido un error al obtener los mensajes de la API"
+                            })
+                        } else {
+                            app.get("logger").info("Los mensajes se listaron correctamente de la API");
+                            res.status(200);
+                            res.logeado = logeado;
+                            res.send(JSON.stringify(mensajes));
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+    // S3 Crear mensaje
     app.post("/api/mensaje/:destino", function (req, res) {
         if (req.token) {
             let criterio = {
@@ -129,37 +180,6 @@ module.exports = function (app, gestorBD) {
         }
     });
 
-    //Ejercicio AS4 Marcar mensaje como leido
-    app.get("api/conversacion",function(req,res){
-        if (req.headers['token']) {
-            let criterio = {
-                $or: [{
-                    emisor: req.session.usuario.email,
-                    remitente: req.params.destino
-                },
-                    {
-                        emisor: req.params.destino,
-                        remitente: req.session.usuario.email
-                    }
-                ]
-            };
-
-            gestorBD.obtenerConversacion(criterio,function (conversaciones){
-                if (conversaciones == null) {
-                    app.get("logger").error("Se ha producido un error al obtener la conversacion");
-                    res.status(500);
-                    res.json({
-                        error: "Se ha producido un error al obtener la conversacion"
-                    })
-                } else {
-                    app.get("logger").info("Los amigos se listaron correctamente de la API");
-                    res.status(200);
-                    res.logeado = logeado;
-                    res.send(JSON.stringify(conversaciones));
-                }
-            });
-        }
-    });
     //Ejercicio AS5 Marcar mensaje como leido
     app.put("/api/mensaje/leido/:id", function (req, res) {
         if (req.token) {
@@ -188,38 +208,5 @@ module.exports = function (app, gestorBD) {
                 }
             });
         }
-    });
-    app.get("/api/mensajes/:id", function (req, res) {
-
-        // Obtener (emisor o receptor es usuario logeado o amigo)
-        let criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
-
-        gestorBD.obtenerConversacion(criterio, function (conversaciones) {
-            if (conversaciones == null || conversaciones.length === 0) {
-                app.get("logger").error("Error al obtener conversación de la API");
-                res.status(500);
-                res.json({
-                    error: "Error al obtener conversación de la API"
-                })
-            } else {
-                let conversacion = conversaciones[0];
-                let criterio = {
-                    conversacion: gestorBD.mongo.ObjectID(conversacion._id)
-                };
-                gestorBD.obtenerMensajes(criterio, function (mensajes) {
-                    if (mensajes == null) {
-                        app.get("logger").error("Error al obtener mensajes de la API");
-                        res.status(500);
-                        res.json({
-                            error: "Error al obtener los mensajes de la API"
-                        })
-                    } else {
-                        app.get("logger").info("Se han obtenido correctamente los mensajes de la API");
-                        res.status(200);
-                        res.send(JSON.stringify(mensajes));
-                    }
-                });
-            }
-        });
     });
 }
