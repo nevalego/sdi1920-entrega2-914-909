acaaec0e0da254b090ed34f819ffe39f232cfa31
Nerea Valdés Egocheaga <nerevalego@gmail.com>
2020-05-11 18:18:34 +0200

    Cliente App Jquery C2 Busqueda de amigos

--- sdi1920-entrega2-914-909/routes/rapiredsocial.js
+++ sdi1920-entrega2-914-909/routes/rapiredsocial.js
@@ -1,43 +1,51 @@
 module.exports = function (app, gestorBD) {
-    app.post("/api/autenticar", function(req, res) {
-       let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
-           .update(req.body.password).digest('hex');
+    app.post("/api/autenticar", function (req, res) {
+        let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
 
         let criterio = {
-            email : req.body.email,
-            password : seguro
+            email: req.body.email,
+            password: seguro
         }
-       gestorBD.obtenerUsuarios(criterio, function (usuarios) {
-            if ( usuarios == null || usuarios.length == 0){
+        gestorBD.obtenerUsuarios(criterio, function (usuarios) {
+            if (usuarios == null || usuarios.length == 0) {
                 res.status(401); // unauthorized
                 res.json({
                     autenticado: false,
                     mensaje: "Error al iniciar de seión"
                 })
-                }else {
+            } else {
                 var token = app.get('jwt').sign(
-                    {usuario: usuarios[0] ,
-                    tiempo: Date.now()/1000},
+                    {
+                        usuario: usuarios[0],
+                        tiempo: Date.now() / 1000
+                    },
                     "secreto");
                 res.status(200);
                 res.json({
-                    autenticado : true,
+                    autenticado: true,
                     token: token
                 })
             }
-       });
+        });
     });
     // S2 Mostrar amigos
     app.get("/api/amigos", function (req, res) {
         if (req.headers['token']) {
+            let buscado = "";
+            let busqueda = false;
+            if (req.query.searchText != null) {
+                busqueda = true;
+                buscado = req.query.searchText;
+            }
             let criterio = {
-                $or:[
+                $or: [
                     {remitente: res.usuario.email},
-                    {emisor :  res.usuario.email}],
+                    {emisor: res.usuario.email}],
                 aceptada: true
             };
             let logeado = res.usuario;
-            gestorBD.obtenerPeticionesDeAmistad(criterio,function (peticiones) {
+            gestorBD.obtenerPeticionesDeAmistad(criterio, function (peticiones) {
                 if (peticiones == null) {
                     app.get("logger").error("Se ha producido un error al obtener las peticiones de la API");
                     res.status(500);
@@ -46,20 +54,28 @@ module.exports = function (app, gestorBD) {
                     })
                 } else {
                     let emails = [];
-                    peticiones.forEach(peticion=>emails.push({"email":peticion.emisor}));
-                    peticiones.forEach(peticion=>emails.push({"email":peticion.remitente}));
-                    let criterioAmigos ={
-                        "$or": emails,
-                        email:{$ne: logeado.email}
-                    };
-                    gestorBD.obtenerUsuarios(criterioAmigos, function (usuarios) {
-                            if( usuarios == null || usuarios.length == 0){
+                    peticiones.forEach(peticion => emails.push({"email": peticion.emisor}));
+                    peticiones.forEach(peticion => emails.push({"email": peticion.remitente}));
+                    let criterioAmigos = {};
+                    if (busqueda) {
+                        criterioAmigos = {
+                            "$or": emails,
+                            email: {$ne: logeado.email},
+                            name: buscado
+                        };
+                    } else {
+                        criterioAmigos = {
+                            "$or": emails,
+                            email: {$ne: logeado.email}
+                        };
+                        gestorBD.obtenerUsuarios(criterioAmigos, function (usuarios) {
+                            if (usuarios == null || usuarios.length == 0) {
                                 app.get("logger").error("Se ha producido un error al obtener los usuarios amigos de la API");
                                 res.status(500);
                                 res.json({
                                     error: "Se ha producido un error al obtener los usuarios amigos de la API"
                                 })
-                            }else{
+                            } else {
                                 app.get("logger").info("Los amigos se listaron correctamente de la API");
                                 res.status(200);
                                 res.logeado = logeado;
@@ -68,9 +84,10 @@ module.exports = function (app, gestorBD) {
                                     usuarios: usuarios
                                 }));
                             }
-                    });
-            }
-        });
+                        });
+                    }
+                }
+            });
         } else {
             res.status(500);
             res.json({
@@ -79,7 +96,7 @@ module.exports = function (app, gestorBD) {
         }
     });
     // S4 Mostrar mensajes de una conversación
-    app.get("/api/conversacion/:id",function(req,res){
+    app.get("/api/conversacion/:id", function (req, res) {
         if (req.headers['token']) {
             // Primero obtener usuario por _id -> amigo.email -> obtener mensajes
             let criterioAmigo = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
@@ -171,20 +188,21 @@ module.exports = function (app, gestorBD) {
     //Ejercicio AS5 Marcar mensaje como leido
     app.put("/api/mensaje/leido/:id", function (req, res) {
         if (req.token) {
-            let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id),
+            let criterio = {
+                "_id": gestorBD.mongo.ObjectID(req.params.id),
                 destino: req.session.usuario.email,
                 leido: false
             };
             let mensaje = {
-                leido:true
+                leido: true
             }
             //Se comprueba primero si los usuarios son amigos
-            gestorBD.modificarMensaje(criterio, mensaje, function(id) {
+            gestorBD.modificarMensaje(criterio, mensaje, function (id) {
                 if (result == null) {
                     app.get("logger").error("Mensaje no existe, o no eres destinatario o ya esta leido");
                     res.status(403);
                     res.json({
-                        error : "Mensaje no existe, o no eres destinatario o ya esta leido"
+                        error: "Mensaje no existe, o no eres destinatario o ya esta leido"
                     })
                 } else {
                     app.get("logger").info("El mensaje se marco correctamente como leido");
@@ -200,14 +218,14 @@ module.exports = function (app, gestorBD) {
 
     //SOLO PARA USO DE TESTS
     //METODO DE BORRADO DE COLECCIONES
-    app.get("/api/eliminarColeccion",function (req, res) {
+    app.get("/api/eliminarColeccion", function (req, res) {
         let coleccion = req.query.coleccion;
         gestorBD.resetBD(coleccion, function (resultado) {
-            if(resultado){
+            if (resultado) {
                 app.get("logger").info('Coleccion borrada exitosamente');
                 res.send("Coleccion borrada exitosamente");
 
-            }else{
+            } else {
                 app.get("logger").error('No se ha podido borrar la coleccion');
                 res.send("No se ha podido borrar la coleccion");
             }
